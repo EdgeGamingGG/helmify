6058df45490b6122b80a7f8f868824729b4804d7
Tamir Daniely <tamir.daniely@ludeo.com>
2025-04-27 16:48:08 +0300

    test nodeselector

--- pkg/processor/pod/pod_test.go
+++ pkg/processor/pod/pod_test.go
@@ -169,6 +169,31 @@ spec:
         operator: "Exists"
         effect: "NoExecute"
 `
+	strDeploymentWithNodeSelector = `
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: nginx-daemonset
+  labels:
+    app: nginx
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.14.2
+        ports:
+        - containerPort: 80
+      nodeSelector:
+        disktype: ssd
+        region: us-east-1
+`
 )
 
 func Test_pod_Process(t *testing.T) {
@@ -422,6 +447,51 @@ func Test_pod_Process(t *testing.T) {
 						"operator": "Exists",
 						"effect":   "NoExecute",
 					},
+				},
+			},
+		}, tmpl)
+	})
+
+	t.Run("daemonset with nodeSelector", func(t *testing.T) {
+		var dae appsv1.DaemonSet
+		obj := internal.GenerateObj(strDeploymentWithNodeSelector)
+		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &dae)
+		specMap, tmpl, err := ProcessSpec("nginx", &metadata.Service{}, dae.Spec.Template.Spec)
+		assert.NoError(t, err)
+
+		assert.Equal(t, map[string]interface{}{
+			"containers": []interface{}{
+				map[string]interface{}{
+					"env": []interface{}{
+						map[string]interface{}{
+							"name":  "KUBERNETES_CLUSTER_DOMAIN",
+							"value": "{{ quote .Values.kubernetesClusterDomain }}",
+						},
+					},
+					"image": "{{ .Values.nginx.nginx.image.repository }}:{{ .Values.nginx.nginx.image.tag | default .Chart.AppVersion }}",
+					"name":  "nginx",
+					"ports": []interface{}{
+						map[string]interface{}{
+							"containerPort": int64(80),
+						},
+					},
+					"resources": map[string]interface{}{},
+				},
+			},
+			"nodeSelector": "{{- toYaml .Values.nginx.nodeSelector | nindent 8 }}",
+		}, specMap)
+
+		assert.Equal(t, helmify.Values{
+			"nginx": map[string]interface{}{
+				"nginx": map[string]interface{}{
+					"image": map[string]interface{}{
+						"repository": "nginx",
+						"tag":        "1.14.2",
+					},
+				},
+				"nodeSelector": map[string]interface{}{
+					"disktype": "ssd",
+					"region":   "us-east-1",
 				},
 			},
 		}, tmpl)
